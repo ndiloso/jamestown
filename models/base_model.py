bea9851e2a4039f2fb4ce807c3bd2442c933ab9f
ndifrekea <ndifreke.anwanakak@gmail.com>
2018-01-03 01:14:44 +0000

    updated postgres

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,26 @@
 import os
 from peewee import Model
 from playhouse.pool import PooledPostgresqlDatabase
+from urllib import parse
+import psycopg2
 
-db_name = os.environ.get("DB_NAME")
-db_host = os.environ.get("DB_HOST")
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
 
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+
+
+
+
+# db_name = os.environ.get("DB_NAME")
+# db_host = os.environ.get("DB_HOST")
+# db_user = os.environ.get("DB_USER")
+# db_password = os.environ.get("DB_PASSWORD")
+
+db_name = url.path[1:]
+db_host = url.hostname
+db_user = url.username
+db_password = url.password
+db_port=url.port
 
 class DBSingelton():
 	db = None
